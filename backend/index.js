d2f0697cbbfb2e0c1fe2ac23e8b8d7608dd1e65b
LimZeWeiDennis <dennislimzw@gmail.com>
2022-12-03 09:43:44 +0800

    Initial Commit

---
+++ backend/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const port = 5001;
+
+app.use(cors());
+
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  console.error(err.message, err.stack);
+  res.status(statusCode).json({ message: err.message });
+  return;
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
