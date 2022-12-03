a99a63a6bd5f5d06f6e55e442a556bd56cffefc1
LimZeWeiDennis <dennislimzw@gmail.com>
2022-12-03 13:03:58 +0800

    Dennis : Updated authentication to account for unfound username

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,28 +1,30 @@
-const db = require("./db")
+const db = require("./db");
 async function user_login(username, password) {
-    
-    const response = await db.query(
-        `SELECT * FROM user WHERE username = "${username}"`,
-    );
+  const response = await db.query(
+    `SELECT * FROM user WHERE username = "${username}"`
+  );
 
-    var user_found = false
-    var correct_password = false
-    var is_loggedin = false
+  var user_found = false;
+  var correct_password = false;
+  var is_loggedin = false;
+  if (response.length === 0) {
+    return { user_found, correct_password, is_loggedin };
+  }
 
-    if (response) {
-        user_found = true 
+  if (response) {
+    user_found = true;
 
-        var password_db = response[0].Password
+    var password_db = response[0].Password;
 
-        if (password != password_db) {
-            return {response: is_loggedin, user_found, correct_password}
-        } else {
-            is_loggedin = true
-            correct_password = true
-            return {response: is_loggedin, user_found, correct_password}
-        }
+    if (password != password_db) {
+      return { user_found, correct_password, is_loggedin };
     } else {
-        return {response: is_loggedin, user_found, correct_password}
+      is_loggedin = true;
+      correct_password = true;
+      return { user_found, correct_password, is_loggedin };
     }
+  } else {
+    return { user_found, correct_password, is_loggedin };
+  }
 }
-module.exports={user_login}
+module.exports = { user_login };
