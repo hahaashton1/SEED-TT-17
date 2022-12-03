de899d0dce7f0d9a231c575be63e4fd83e4f9cd0
LimZeWeiDennis <dennislimzw@gmail.com>
2022-12-03 14:37:44 +0800

    Dennis: Updated user controller to return row of user content

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -6,27 +6,27 @@ async function user_login(username, password) {
   );
 
   var user_found = false;
-  var correct_password = false;
   var is_loggedin = false;
-  if (response.length === 0) {
-    return { user_found, correct_password, is_loggedin };
-  }
+  var userObject = {};
 
-  if (response) {
+  if (response.length === 0) {
+    return { user_found, is_loggedin, userObject };
+  } else {
     user_found = true;
 
     var password_db = response[0].Password;
 
     if (password != password_db) {
-      return { user_found, correct_password, is_loggedin };
+      return { user_found, is_loggedin, userObject };
     } else {
       is_loggedin = true;
-      correct_password = true;
-      return { user_found, correct_password, is_loggedin };
+      userObject = response[0];
+      return { user_found, is_loggedin, userObject };
     }
-  } else {
-    return { user_found, correct_password, is_loggedin };
   }
+  //   } else {
+  //     return { user_found, correct_password, is_loggedin };
+  //   }
 }
 
 async function get_address(user_id) {
