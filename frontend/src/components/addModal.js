0f02fb7fd00e76be95efd215db31ec5fc58c1392
Tan Wen Xiu <wenxiu1303@gmail.com>
2022-12-03 16:33:55 +0800

    Revert "modal update"
    This reverts commit 3b0838641195618201520aec6ae53dca6cba68ff.

--- frontend/src/components/addModal.js
+++ frontend/src/components/addModal.js
@@ -1,5 +1,5 @@
 import React from "react";
-import './Dashboard.css';
+import './Dashboard.css'; 
 import { Button, Modal, Form, Dropdown, Container} from "react-bootstrap";
 
 const AddModal = (props) => {
@@ -10,31 +10,42 @@ const AddModal = (props) => {
             </Modal.Header>
             <Modal.Body>
                 <Form>
-                    {/* <Form.Group>
+                    <Form.Group>
                         <Form.Label>Account ID</Form.Label>
-                        <Form.Control type="text" placeholder="Enter Account ID" />
-                    </Form.Group> */}
+                        <Container fluid="md">
+                            <Dropdown>
+                                <Dropdown.Toggle>
+                                    Accounts
+                                </Dropdown.Toggle>
+
+                                <Dropdown.Menu>
+                                    <Dropdown.Item href="#/account-1">Account 1</Dropdown.Item>
+                                    <Dropdown.Item href="#/account-2">Account 2</Dropdown.Item>
+                                    <Dropdown.Item href="#/account-3">Account 3</Dropdown.Item>
+                                </Dropdown.Menu>
+                            </Dropdown>
+                        </Container>
+                    </Form.Group>
+                    {/* <Form.Label>Account ID</Form.Label>
+                        <Form.Control type="text" placeholder="Enter Account ID" />*/}
                     <Form.Group>
                         <Form.Label>Receiving Account ID</Form.Label>
                         <Form.Control type="text" placeholder="Enter Receiving Account ID" />
                     </Form.Group>
-                    <br/>  
                     <Form.Group>
                         <Form.Label>Date</Form.Label>
                         <Form.Control type="text" placeholder="Enter a date" />
                     </Form.Group>
-                    <br/>  
                     <Form.Group>
                         <Form.Label>Transaction Amount</Form.Label>
                         <Form.Control type="number" placeholder="Enter transaction amount" />
                     </Form.Group>
-                    <br/>  
                     <Form.Group>
                         <Form.Label>Comment</Form.Label>
                         <Form.Control type="text" placeholder="Enter comments" />
                     </Form.Group>
                     <br/>
-                    <Button className="add-button" variant="success">
+                    <Button variant="primary">
                         Submit
                     </Button>
                 </Form>
