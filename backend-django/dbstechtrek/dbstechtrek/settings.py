ec900085a790731ee94cf39c998ceb380028e109
jackysim <1902609@sit.singaporetech.edu.sg>
2022-12-03 11:02:03 +0800

    sample model and db change

--- backend-django/dbstechtrek/dbstechtrek/settings.py
+++ backend-django/dbstechtrek/dbstechtrek/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'dbstechtrek_webapp.apps.DbstechtrekWebappConfig'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,9 @@ WSGI_APPLICATION = 'dbstechtrek.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'USER': 'root',
+        'NAME': 'Bank',
     }
 }
 
--- backend-django/dbstechtrek/dbstechtrek_webapp/models.py
+++ backend-django/dbstechtrek/dbstechtrek_webapp/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 
 # Create your models here.
-class Transactions(models.Model):
-    xxx
+class User(models.Model):
+    username = models.CharField(max_length=20)
+    password = models.CharField(max_length=20)
+    firstname = models.CharField(max_length=255, verbose_name="FirstName")
+    lastname = models.CharField(max_length=255, verbose_name="LastName")
+    email = models.CharField(max_length=255, verbose_name="Email")
+    address = models.CharField(max_length=255, verbose_name="Address")
