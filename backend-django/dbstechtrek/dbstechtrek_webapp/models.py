17fadf375244794038919443ab6c0c631fe1123d
AugustusLow <e0426026@u.nus.edu>
2022-12-03 11:53:06 +0800

    Update models.py

--- backend-django/dbstechtrek/dbstechtrek_webapp/models.py
+++ backend-django/dbstechtrek/dbstechtrek_webapp/models.py
@@ -10,3 +10,17 @@ class User(models.Model):
     email = models.CharField(max_length=255, verbose_name="Email")
     address = models.CharField(max_length=255, verbose_name="Address")
     optintophystatements = models.IntegerField(max_length=1, verbose_name="OptIntoPhyStatements")
+
+class BankAccount(models.Model):
+    accountid = models.IntegerField(primary_key=True)
+    userid = models.ForeignKey('User')
+    accounttype = models.CharField(max_length=255, verbose_name= "AccountType")
+    accountbalance = models.DecimalField(max_digits=10, decimal_places=2)
+
+class ScheduledTransactions(models.Model):
+    transactionid = models.IntegerField(primary_key=True)
+    receivingaccountid = models.IntegerField()
+    date = models.CharField(max_length=255, verbose_name="Date")
+    transactionammount = models.DecimalField(max_length=10,decimal_places=2)
+    comment = models.CharField(max_length=255, verbose_name="Comment")
+    accountid = models.ForeignKey('BankAccount')
