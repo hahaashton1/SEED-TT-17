6e5d2c23d264ab9e1bc0e422d04ba19da45842ab
jackysim <1902609@sit.singaporetech.edu.sg>
2022-12-03 11:44:38 +0800

    db seed data sample

--- backend-django/dbstechtrek/dbstechtrek_webapp/models.py
+++ backend-django/dbstechtrek/dbstechtrek_webapp/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
+    userid = models.IntegerField(max_length=200, verbose_name="UserID", )
     username = models.CharField(max_length=20)
     password = models.CharField(max_length=20)
     firstname = models.CharField(max_length=255, verbose_name="FirstName")
