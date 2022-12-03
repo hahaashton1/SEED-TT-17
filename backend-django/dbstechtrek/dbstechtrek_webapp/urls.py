2fe51ae9c18fc6e2c53f9f5665f395fb1d7d4c22
jackysim <1902609@sit.singaporetech.edu.sg>
2022-12-03 14:30:19 +0800

    comment out url

--- backend-django/dbstechtrek/dbstechtrek_webapp/urls.py
+++ backend-django/dbstechtrek/dbstechtrek_webapp/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('/viewTransactionDetails', views.viewTransactionDetails, name='viewTransactionDetails')
+    # path('/viewTransactionDetails', views.viewTransactionDetails, name='viewTransactionDetails')
 ]
